ccff0947ca1c426e4f023b2d965fc5a6baba9bd3
Kapil <kapilchandra81@gmail.com>
2016-08-08 00:57:56 +0530

    Commenting out the code and making small changes

--- adi2d.f90
+++ adi2d.f90
@@ -1,32 +1,38 @@
+!================================================================================================================
+!TRANSIENT SOLUTION IN 2-D WITH 4 TEMPERATURE BOUNDARY CONDITIONS USING ALTERNATE DIRECTION IMPLLCIT(ADI) METHOD
+!Solved for 1-(size1-1) in both x- and y- direction
+!T(time,x,y)
+!================================================================================================================
 program adi
   implicit none
 
   real :: tk,lambda,del_x,del_t,length,time
   integer :: i,j,k,l,size1,size2,n
-  real, allocatable, dimension(:,:,:) :: T
-  real, allocatable, dimension(:,:) :: M,T1
-  real, allocatable,dimension(:) :: P,buff
+  real, allocatable, dimension(:,:,:) :: T !Temperature variable
+  real, allocatable, dimension(:,:) :: M,T1 !T1--Variable to facilitate explicit method; M--Constant matrix for implicit method
+  real, allocatable,dimension(:) :: P,buff !P--Array containing constants; buff--Matrix to take solution from gauss subroutine
 
   !Inputs from the user
   print *,"enter delta x:"
   read *,del_x ! For delta x
 
-  !print *,"enter delta t:"
-  read *,size2 ! For delta t
+  print *,"enter delta t:"
+  read *,del_t ! For delta t
 
   print *,"enter length of the rod:"
   read *,length
 
-  !print *,"enter the time period:"
-  !read *,time
+  print *,"enter the time period:"
+  read *,time
 
-  print *,"enter thermal diffusivity:"
+  print *,"enter lambda:"
   read *,lambda
 
   !lambda = tk*del_t/(del_x**2)
   size1 = int(length/del_x)+1
-  !size2 = 2*int(time/del_t)
+  size2 = 2*int(time/del_t) !Multiplied by 2 because in the code time increments by 1 for each direction
 
+  !Allocation of memory
   allocate(T(0:size2,0:size1,0:size1))
   allocate(T1(0:size1,0:size1))
   allocate(M(size1-1,size1-1))
@@ -39,7 +45,6 @@ program adi
       M(i,j) = 0
     end do
   end do
-
   do i = 1,size1-1
     M(i,i) = 2*(1+lambda)
   end do
@@ -59,6 +64,7 @@ do i = 2,size1-1
   T(0:size2,1:size1-1,size1) = 100 !Top surface
   T(0:size2,1:size1-1,0) = 0 !Bottom surface
 
+  !Initialising all the values in the temperature matrix to 0 so that it doens't take junk values
   do i = 1,size2
     do j = 1,size1-1
       do k = 1,size1-1
@@ -67,12 +73,12 @@ do i = 2,size1-1
     end do
   end do
 
+  !Equating T1 to T
   do i = 1,size1-1
     do k = 1,size1-1
       T1(i,k) = T(0,i,k)
     end do
   end do
-
   T1(0,1:size1-1) = 75 !Left surface
   T1(size1,1:size1-1) = 50 !Right surface
   T1(1:size1-1,size1) = 100 !Top surface
@@ -80,13 +86,17 @@ do i = 2,size1-1
 
 
   do l = 1, size2
+    !Updating the values of T
     do i = 0,size1
       do j = 0,size1
         T(l,i,j) = T(l-1,i,j)
       end do
     end do
+
+    !FIRST DIRECTION
     if (MOD(l,2) .ne. 0) then
     do i = 1,size1-1
+    !Making the constant matrix on the right side of the eqaution
     P(1) = lambda*T1(i-1,1) + 2*(1-lambda)*T1(i,1) + lambda*T1(i+1,1) + lambda*T1(i,0)
     P(size1-1) = lambda*T1(i-1,size1-1) + 2*(1-lambda)*T1(i,size1-1) + lambda*T1(i+1,size1-1) + lambda*T1(i,size1)
     do k = 2,size1-2
@@ -104,15 +114,18 @@ do j = 0,size1
   end do
 end do
 
-  end do
+  end do !End of i loop
 
+!SECOND DIRECTION
 else
+  !Updating the values of T
   do i = 0,size1
     do j = 0,size1
       T(l,i,j) = T(l-1,i,j)
     end do
   end do
   do i = 1,size1-1
+  !Making the constant matrix on the right side of the eqaution
   P(1) = lambda*T1(1,i-1) + 2*(1-lambda)*T1(1,i) + lambda*T1(1,i+1) + lambda*T1(0,i)
   P(size1-1) = lambda*T1(size1-1,i-1) + 2*(1-lambda)*T1(size1-1,i) + lambda*T1(size1-1,i+1) + lambda*T1(size1,i)
   do k = 2,size1-2
@@ -130,19 +143,11 @@ do k = 0,size1
 end do
 end do
 
-if (i .eq. 1) then
-do j = 1,size1-1
-do k = 1,size1-1
-  !print *,T1(j,k)
-end do
-end do
+end do !End of i loop
 end if
 
-
-end do
-end if
-
-if (l .eq. 20) then
+!Code to check answer
+if (l .eq. 2) then
 do j = 1,size1-1
   do i = 1,size1-1
     print *,T(l,i,j)
@@ -150,10 +155,13 @@ do j = 1,size1-1
 end do
 end if
 
-end do
+end do !End of time loop
 
 end program
 
+!================================
+!SUBROUTINE FOR GAUSS METHOD
+!================================
 subroutine gauss(A,B,n,X)
   implicit none
   real, dimension(n,n) :: A,A1
@@ -207,3 +215,4 @@ subroutine gauss(A,B,n,X)
 !print *,X
 
 end subroutine
+
